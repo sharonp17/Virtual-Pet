9ef2ae8061f6df1d63715a1aebd4639487d3dc96
sharonp <sfacm86@gmail.com>
2017-02-25 09:33:33 -0500

    First commit

---
+++ DigiPet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualPet martian = new VirtualPet();
+
+            Console.WriteLine(" Welcome to your Virtual Martian Pet!" );
+            Console.WriteLine();
+            Console.WriteLine("Congratulations! You're now the owner of marvelous Martian Panda");
+            Console.WriteLine();
+            Console.WriteLine(" Let's begin by giving your pet a Name!");
+            string userPetName = Console.ReadLine();
+            Console.WriteLine();
+            martian.Name = userPetName;
+
+            //Console.WriteLine(martian.Name);
+
+            Console.WriteLine(@"Now you can choose their color from one of the following ""Rainbow"", ""Invisible"" or "" Black and White"" ");
+
+            string[] color = { "Rainbow", "Invisible", "Black and White" };
+            string martianColor = Console.ReadLine();
+
+            if (martianColor == color)
+            {
+
+            }
+
+
+
+        }
+    }
+}
---
+++ DigiPet/VirtualPet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiPet
+{
+    class VirtualPet
+    {
+//fields
+        private string petName;
+        private string color;
+        private int petAge;
+        private bool hunger; //should these be float? set to 0-5?
+        private bool tired;
+        private bool bored;
+        private bool thirst;
+
+//Constructors
+        public VirtualPet(string petName, string color, int petAge)
+        {
+            this.petName = Name;
+            this.color = Color;
+            this.petAge = petAge;
+
+            
+        }
+        public VirtualPet(bool hunger, bool tired, bool bored, bool thirst)
+        {
+            this.hunger = hunger;
+            this.tired = tired;
+            this.bored = bored;
+            this.thirst = thirst;
+        }
+
+        public VirtualPet()
+        {
+
+        }
+// Properties
+        public string Name
+        {
+            get { return this.petName; }
+            set { this.petName = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+           
+        }
+
+        public int PetAge
+        {
+            get { return this.petAge; }
+            set { this.petAge = value; }
+           
+        }
+//
+        public void PetInfo()
+        {
+            Console.WriteLine(this.petName);
+            Console.WriteLine(this.color);
+            Console.WriteLine(this.petAge);
+        }
+
+        public void CurrentState()
+        {
+            //Console.WriteLine("Hunger" +);
+            //Console.WriteLine("Fatigue" +);
+            //Console.WriteLine("Boredome" +);
+            //Console.WriteLine("Thirst" +);
+        }
+    }
+}
+
+
