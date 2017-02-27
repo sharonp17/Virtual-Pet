69858e5eec9b32c34295f735a590e70833a135b9
sharonp <sfacm86@gmail.com>
2017-02-27 10:54:41 -0500

    first commit

--- DigiPet/Program.cs
+++ DigiPet/Program.cs
@@ -10,6 +10,8 @@ namespace DigiPet
     {
         static void Main(string[] args)
         {
+// Virtual Pet is a digital interaction program that allows you to engage with the program in 4 ways.
+
             VirtualPet martian = new VirtualPet();
             int userChoice;
 
@@ -17,12 +19,15 @@ namespace DigiPet
             Console.WriteLine();
             Console.WriteLine("Congratulations! You're now the owner of marvelous Martian Panda!");
             Console.WriteLine();
-            Console.WriteLine("Let's begin by giving your pet a Name!");
+            Console.WriteLine(" Let's begin by giving your pet a Name!");
             string userPetName = Console.ReadLine();
             Console.WriteLine();
             martian.Name = userPetName;
 
-            //Console.WriteLine(martian.Name);
+//After you have named your pet, you are prompted with an updated menue that shows the current status of your digital pet in the 4
+//established categories of (Hunger, Fatigue, Boredome and Thirst). These values are established in bool so the status will only ever
+//show the status in true/ false scenarios. i.g. hungry/not hungry, tired/ not tired. etc. The program is wrapped in a do-while loop 
+//to keep return the menu after each user entry. 
             do
             {
                 martian.Tick();
@@ -35,6 +40,9 @@ namespace DigiPet
                 Console.WriteLine("Boredome = {0} ", martian.Boredom);
                 Console.WriteLine("Thirst = {0} ", martian.Thirst);
                 Console.WriteLine();
+//depending on whether or not the current status of the pet is true or false the user entry will have different effects. for example,
+//if the pet IS hungry and the user feeds the pet, the pet will accept the food. If the pet is NOT hungry the pet will tell the user
+//that they are not hungry.
 
                 Console.WriteLine("Type 1 : Feed \nType 2 : Put to bed \nType 3 : To Play with \nType 4 : to give water \n Press 5 to quit");
                 userChoice = int.Parse(Console.ReadLine());
@@ -85,7 +93,7 @@ namespace DigiPet
                     }
                 }
 
-
+//After each entry the interaction will clear from the screen and a new menu/updated status will show.
                 Console.Clear();
 
             }
